267b0fcea5e6afa4d899539fb56e94655670932d
codrinunici <44782545+codrinunici@users.noreply.github.com>
2019-10-20 20:30:24 +0300

    Working qr
    we did it boi

--- app/src/main/java/com/example/huntknow/HomeActivity.kt
+++ app/src/main/java/com/example/huntknow/HomeActivity.kt
@@ -4,29 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class HomeActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val openScan = findViewById<Button>(R.id.open_scan)
-        openScan.setOnClickListener {
+        val setQrResult : TextView = findViewById(R.id.qrResultTest)
+
+        var getScanResult: String
+
+        val intent = intent
+        val bundle = intent.extras
+
+        if (bundle != null) {
+            getScanResult = bundle.getString("qrResult")!!
+            setQrResult.text=getScanResult.toString()
+        }
+
+        val goToQRScan: Button = findViewById(R.id.open_scan)
+        goToQRScan.setOnClickListener {
             val intent = Intent(this, ScanActivity::class.java)
             startActivity(intent)
         }
-        val openLeaderboard : Button = findViewById(R.id.open_leaderboard)
+        val openLeaderboard: Button = findViewById(R.id.open_leaderboard)
         openLeaderboard.setOnClickListener {
             val intent = Intent(this, LeaderboardActivity::class.java)
             startActivity(intent)
-        }
 
-        val openLocation : Button = findViewById(R.id.open_location)
-        openLocation.setOnClickListener {
-            val intent = Intent(this, LocationActivity::class.java)
-            startActivity(intent)
         }
+            val openLocation: Button = findViewById(R.id.open_location)
+            openLocation.setOnClickListener {
+                val intent = Intent(this, LocationActivity::class.java)
+                startActivity(intent)
+            }
 
 
     }
